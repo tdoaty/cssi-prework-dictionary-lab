dbed7f97ff96eae53daff3f65f072741b1ba5573
tdoaty <tdoaty@google.com>
2017-05-17 15:17:09 -0400

    all testts passed

--- basketball.py
+++ basketball.py
@@ -16,8 +16,8 @@ def player_points(team_name):
     player_info = ""
     for player in game_dictionary[team_name]['players']:
         player_info += player['name'] + " - " + str(player['points']) + " pts, "
-        player_info = player_info[:-2]
-        player_info += "."
+    player_info = player_info[:-2]
+    player_info += "."
     return player_info
 
 #This method should return all of the statistics for a player, given that players team and name
@@ -26,14 +26,14 @@ def player_stats(team, player_name):
     for player in game_dictionary[team]['players']:
         if player['name'] == player_name:
             stats += (
-            "Name: " + player_name + "/n"+
-            "Slam_dunks: " + str(player['slam_dunks']) + "/n" +
-            "Number: " + str(player['points']) + "/n" +
-            "Points: " + str(player['shoe']) + "/n" +
+            "Name: " + player_name + "\n"+
+            "Slam_dunks: " + str(player['slam_dunks']) + "\n" +
+            "Number: " + str(player['number']) + "\n" +
+            "Points: " + str(player['points']) + "\n" +
+            "Shoe: " + str(player['shoe']) + "\n" +
             "Steals: " + str(player['steals'])
             )
     return stats
-
 
 
 # Return a player's shoe size.
@@ -46,3 +46,8 @@ def shoe_size(team, player_name):
 
 # Find the player on the team with the biggest shoe size and return their steals for the game
 def big_shoe_stealer(team):
+    current_biggest_shoe_size_player = game_dictionary[team]['players'][0]
+    for player in game_dictionary[team]['players']:
+        if player['shoe'] > current_biggest_shoe_size_player['shoe']:
+            current_biggest_shoe_size_player = player
+    return current_biggest_shoe_size_player['steals']
